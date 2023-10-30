fe64b4826cc602b88429af87a575ba1ab16e387e
Kevin <briand-kevin@hotmail.fr>
2023-10-30 11:47:55 +0100

    feat: change users list display
    Changed the display of the user list on OBS, now the list automatically scrolls to the bottom after a few seconds.

--- src/web-server/src/components/UsersList.tsx
+++ src/web-server/src/components/UsersList.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Row } from './Row';
 import { UserDto } from '../../../renderer/pages/waiting-list/components/list/dto/user.dto';
 import Live from './Live';
@@ -12,10 +12,10 @@ type StyledListProps = {
 };
 
 const StyledList = styled.ul<StyledListProps>`
-  display: flex;
-  flex-direction: column;
   width: 100%;
   height: 100%;
+  overflow-y: hidden;
+  transition: transform 30s linear;
   animation: ${(props) => (props.$show ? 'show' : 'hide')} 1s forwards;
 
   @keyframes show {
@@ -38,9 +38,11 @@ const StyledList = styled.ul<StyledListProps>`
 `;
 
 const EmptyList = styled.div`
-  align-self: center;
-  vertical-align: middle;
-  margin: auto;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   color: white;
   font-size: 2.5rem;
 `;
@@ -50,9 +52,15 @@ type ListProps = {
 };
 
 function UsersList({ users }: ListProps) {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const SCROLL_INCREMENT = 1;
+  const TIME_BEFORE_SCROLL_IN_SECONDS = 5;
+  const TIME_BEFORE_SHOW_LIST_IN_SECONDS = 1.5;
   const [showList, setShowList] = useState<boolean>(true);
   const { t } = useTranslation('translation');
+  const usersList = useRef<HTMLUListElement>(null);
+  const [isScrolling, setIsScrolling] = useState(true);
+  const animationFrameId = useRef<number>();
+  const scrollY = useRef<number>(0);
 
   const statusPrefix = (user: UserDto) => {
     if (user.status === UserStatus.LIVE) {
@@ -61,51 +69,78 @@ function UsersList({ users }: ListProps) {
     return <Next />;
   };
 
-  const getNextUsers = () => {
-    const nextUsers = users.slice(currentIndex, currentIndex + 4);
+  // Hide the list, scroll to top and show
+  const resetListPos = () => {
+    const list = usersList.current;
+    if (!list) {
+      return () => {};
+    }
 
-    return nextUsers.map((user) => {
-      return (
+    // reset position
+    setIsScrolling(false);
+    list.scrollTop = 0;
+    scrollY.current = 0;
+
+    setShowList(true);
+    // wait end show animation
+    const timer = setTimeout(() => {
+      setIsScrolling(true);
+    }, TIME_BEFORE_SCROLL_IN_SECONDS * 1000);
+
+    return () => clearTimeout(timer);
+  };
+
+  // scroll the list to the bottom
+  const scrollToBottom = useCallback(() => {
+    const scrollElement = usersList.current;
+    if (!scrollElement) {
+      return;
+    }
+
+    const maxScroll = scrollElement.scrollHeight - scrollElement.clientHeight;
+    if (maxScroll === 0) {
+      return;
+    }
+    // when we are at the end of the list
+    if (scrollY.current >= maxScroll) {
+      setShowList(false);
+      setTimeout(() => {
+        resetListPos();
+      }, TIME_BEFORE_SHOW_LIST_IN_SECONDS * 1000);
+      return;
+    }
+
+    scrollY.current += SCROLL_INCREMENT;
+    scrollElement.scrollTop = scrollY.current;
+    animationFrameId.current = requestAnimationFrame(scrollToBottom);
+  }, []);
+
+  // start scroll animation
+  useEffect(() => {
+    if (isScrolling) {
+      animationFrameId.current = requestAnimationFrame(scrollToBottom);
+    }
+    return () => {
+      if (animationFrameId.current) {
+        cancelAnimationFrame(animationFrameId.current);
+      }
+    };
+  }, [isScrolling, scrollToBottom]);
+
+  // reset list pos if users change
+  useEffect(() => {
+    resetListPos();
+  }, [users]);
+
+  return (
+    <StyledList $show={showList} ref={usersList}>
+      {users.map((user) => (
         <Row
           key={user.username}
           name={user.username ?? ''}
           statusPrefix={statusPrefix(user)}
         />
-      );
-    });
-  };
-
-  const hideDisplayList = useCallback(() => {
-    if (users.length === 0 || (users.length < 5 && currentIndex === 0)) {
-      return;
-    }
-    if (showList) {
-      setShowList(false);
-      return;
-    }
-    setCurrentIndex((prevState) =>
-      prevState + 4 < users.length ? prevState + 4 : 0
-    );
-  }, [currentIndex, showList, users.length]);
-
-  useEffect(() => {
-    if (users.length < 5 && currentIndex > 0) {
-      setCurrentIndex(0);
-    }
-    // Start timer to move the next page
-    const timerId = setTimeout(hideDisplayList, showList ? 10000 : 2000);
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [currentIndex, hideDisplayList, showList, users]);
-
-  useEffect(() => {
-    setShowList(true);
-  }, [currentIndex]);
-
-  return (
-    <StyledList $show={showList}>
-      {getNextUsers()}
+      ))}
       {users.length === 0 ? <EmptyList>{t('emptyList')}</EmptyList> : null}
     </StyledList>
   );
